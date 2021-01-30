d50cc43656480aecd592334a3dc642c59aa57d22
shrinivasshah <shrinivasshah05@gmail.com>
2021-01-30 07:20:47 +0530

    finished with typing comparison

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  inputValue = '';
+  showSuccess = false;
+
+  onInput(value: string) {
+    this.inputValue = value;
+    console.log(this.inputValue);
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
